860dd880158cbf4f422ea86646d8599581ee7b0b
Wessel Dankers <wsl@fruit.je>
2014-05-18 15:08:13 +0200

    raii locking dingie

---
+++ lib/Fruitbak/Transfer/Rsync/Lock.pm
@@ -0,0 +1,49 @@
+=encoding utf8
+
+=head1 NAME
+
+Fruitbak::Transfer::Rsync::Lock - locking between processes
+
+=head1 AUTHOR
+
+Wessel Dankers <wsl@fruit.je>
+
+=head1 COPYRIGHT
+
+Copyright (c) 2014  Wessel Dankers <wsl@fruit.je>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+=cut
+
+package Fruitbak::Transfer::Rsync::Lock;
+
+use autodie;
+
+use Fcntl qw(:flock);
+
+use Class::Clarity -self;
+
+field lockfh;
+
+sub new() {
+	my $self = super;
+	flock($self->lockfh, LOCK_EX) or die;
+	return $self;
+}
+
+sub DESTROY {
+	flock($self->lockfh, LOCK_UN);
+}
