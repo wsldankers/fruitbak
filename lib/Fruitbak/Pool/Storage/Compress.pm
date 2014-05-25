43d3e849453741dbd8ec3bea2886d222e73ca21e
Wessel Dankers <wsl@fruit.je>
2014-05-25 20:06:10 +0200

    add a compression filter

---
+++ lib/Fruitbak/Pool/Storage/Compress.pm
@@ -0,0 +1,52 @@
+=encoding utf8
+
+=head1 NAME
+
+Fruitbak::Pool::Storage::Compress - allow for pooled data to be compressed
+
+=head1 AUTHOR
+
+Wessel Dankers <wsl@fruit.je>
+
+=head1 COPYRIGHT
+
+Copyright (c) 2014  Wessel Dankers <wsl@fruit.je>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+=cut
+
+package Fruitbak::Pool::Storage::Compress;
+
+use IO::Compress::Gzip qw(gzip $GzipError);
+use IO::Uncompress::Gunzip qw(gunzip $GunzipError);
+
+use Fruitbak::Pool::Storage::Filter -self;
+
+field level => sub { $self->cfg->{level} // 1 };
+
+sub apply {
+	my ($hash, $data) = @_;
+	gzip($data, \my $res, -Level => $self->level)
+		or die "compression failed: $GzipError\n";
+	return \$res;
+}
+
+sub unapply {
+	my ($hash, $data) = @_;
+	gunzip($data, \my $res)
+		or die "decompression failed: $GunzipError\n";
+	return \$res;
+}
---
+++ lib/Fruitbak/Pool/Storage/Filter.pm
@@ -0,0 +1,64 @@
+=encoding utf8
+
+=head1 NAME
+
+Fruitbak::Pool::Storage::Filter - base class for storage filters
+
+=head1 METHODS
+
+=head2 apply($hash, $data)
+
+Apply the filter to the data (a scalar ref) and return the modified
+data (also as a scalar ref).
+
+=head2 unapply($hash, $data)
+
+Like apply, but should remove the data transformation.
+
+=head1 AUTHOR
+
+Wessel Dankers <wsl@fruit.je>
+
+=head1 COPYRIGHT
+
+Copyright (c) 2014  Wessel Dankers <wsl@fruit.je>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+=cut
+
+package Fruitbak::Pool::Storage::Filter;
+
+use Fruitbak::Pool::Storage -self;
+
+field subpool => sub {
+	return $self->pool->instantiate_storage($self->cfg->{pool} // ['filesystem']);
+};
+
+sub store {
+	my ($hash, $data) = @_;
+	$data = $self->apply($hash, $data);
+	return $self->subpool->store($hash, $data);
+}
+sub retrieve {
+	my $hash = shift;
+	my $data = $self->subpool->retrieve($hash);
+	return $self->unapply($hash, $data);
+}
+sub has { return $self->subpool->has(@_) }
+sub remove { return $self->subpool->remove(@_) }
+
+stub apply;
+stub unapply;
