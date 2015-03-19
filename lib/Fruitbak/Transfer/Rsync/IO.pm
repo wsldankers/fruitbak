541ee9b01cffd68aa0e2ba246fa922b2400b0c85
Wessel Dankers <wsl@fruit.je>
2015-03-19 22:47:50 +0100

    more docs for Fruitbak::Transfer::Rsync::IO

--- lib/Fruitbak/Transfer/Rsync/IO.pm
+++ lib/Fruitbak/Transfer/Rsync/IO.pm
@@ -30,8 +30,8 @@ Fruitbak::Transfer::Rsync::IO object to spring into existence. This is, in
 fact, one of the main reasons to run this code in a seperate process with
 an RPC tether.
 
-The important consequences of forking for our RPC channel is that multiple
-processes may be reading from and writing to the channel at the same time.
+One important consequence of forking for our RPC channel is that multiple
+processes will be reading from and writing to the channel at the same time.
 That means we have to maintain a lock. Because it is well-defined across
 forks, flock()-based locking was selected for this job.
 
@@ -39,14 +39,20 @@ We assume that the input and output file handles are POSIX pipes. This
 means writes of up to PIPE_BUF bytes are atomic. We can exploit that
 feature to maximize parallellism by using both shared and exclusive locks.
 
-Exclusive locks are needed when the write is non-atomic, and/or the RPC
-call requires a reply. When the write is atomic and no reply is required,
-we can obtain a shared lock. This will excluded the processes that are
-sending complicated calls while all processes that send simple calls can
-process those in parallel.
+Exclusive locks are needed when the write is non-atomic or the RPC call
+requires a reply. When the write is atomic and no reply is required, we can
+obtain a shared lock. This will exclude the processes that are sending
+complicated calls while processes that send simple calls can send those in
+parallel.
 
 The wire protocol is described in more detail in
 Fruitbak::Transfer::Rsync::RPC.
+
+=head1 CONSTRUCTOR
+
+Required arguments to the constructor are "in", "out" and "lockname". If
+you happen to have an open file handle for the lockfile, you can pass it in
+"lockfh" (but in that case you must also pass lockpid => $$).
 
 =cut
 
@@ -105,87 +111,138 @@ This file should persist during the rsync transfer run, so keep it around
 in a seperate variable while File::RsyncP is running. Do not change after
 initialization.
 
+If you pass a lockfh to the initializer, you must also pass lockpid (set to
+$$).
+
 =cut
 
-field lockfh;
+field lockfh => sub {
+	open my $lockfh, '+<', $self->lockname
+		or die "can't open $lockname: $!\n";
+	$self->lockpid($pid);
+	return $lockfh;
+};
 
 =item lockname
 
-The filename of the lockfile. If not explicitly set, will be determined by
-calling lockfh->filename. File::Temp objects support this method. Do not
-change after initialization.
+The filename of the lockfile. This is a required argument to the
+constructor. Do not change after initialization.
 
 =cut
 
-field lockname => sub { $self->lockfh->filename };
+field lockname;
 
 =item lockpid
 
-The pid of the process that initially opened the lockfile. Should usually
-simply be $$.
+The pid of the process that opened the current lockfh. Should usually
+simply be $$. If the object is trying to obtain a lock but its process ID
+does not match this value, it assumes that a fork has taken place and the
+lock file needs to be reopened.
 
 =cut
 
 field lockpid => 0;
+
+=item in
+
+The file descriptor from which replies are read. Required argument to the
+constructor.
+
+=cut
+
 field in;
+
+=item out
+
+The file descriptor to which RPC calls are dispatched. Required argument to
+the constructor. Must be a pipe.
+
+=cut
+
 field out;
+
+=back
+
+=head METHODS
+
+=over
+
+=item version
+
+The File::RsyncP API version that we implement. Hardcoded to 2.
+
+=cut
 
 use constant version => 2;
 
-sub raiilock {
-	my $pid = $$;
+=item guardlock
+
+Takes a lock on the lockfile and returns an object that, when discarded,
+unlocks the lockfile again. If the registered process ID does not match the
+current one, the lockfile is reopened.
+
+=cut
+
+sub guardlock {
 	my $shared = shift;
-	my $lockname = $self->lockname;
-	if($self->lockfh_isset && $self->lockpid == $pid) {
-		my $lockfh = $self->lockfh;
-		flock($lockfh, $shared ? LOCK_SH : LOCK_EX)
-			or die "flock($lockname): $!\n";
-		return guard { flock($lockfh, LOCK_UN) or die "flock($lockname): $!\n" };
+	$self->lockfh_unset if $self->lockpid != $$;
+	my $lockfh = $self->lockfh;
+	unless(flock($lockfh, $shared ? LOCK_SH : LOCK_EX)) {
+		my $lockname = $self->lockname;
+		die "flock($lockname): $!\n";
 	}
-	open my $lockfh, '+<', $lockname
-		or die "can't open $lockname: $!\n";
-	$self->lockfh($lockfh);
-	$self->lockpid($pid);
-	return guard { flock($lockfh, LOCK_UN) or die "flock($lockname): $!\n" };
+	return guard { flock($lockfh, LOCK_UN) };
 }
+
+=item dirs
+
+Dummy function that accepts and discards the local and remote directory.
+
+=cut
 
 sub dirs {}
 
+=item send_rpc
+
+Sends an RPC call to the "out" file handle, using the appropriate locking
+type depending on the size of the request. Throws an exception if an error
+occurs.
+
+=cut
+
 sub send_rpc {
+	confess("internal error: utf8 data passed") if utf8::is_utf8($_[1]);
 	my $buf = pack('LC', length($_[1]), $_[0]).$_[1];
 	if(length($buf) > PIPE_BUF) {
-		my $lock = $self->raiilock;
+		my $lock = $self->guardlock;
 		my $r = syswrite($self->out, $buf);
 		die "write(): $!\n" unless defined $r;
 		# POSIX guarantees that no partial writes will occur
-		confess "short write"
-			if $r < length($buf);
+		confess "short write" if $r < length($buf);
 	} else {
 		# POSIX guarantees that writes of up to PIPE_BUF bytes are atomic.
 		# However, even if this write is atomic, we might interrupt a larger
 		# non-atomic write. We therefore still have to take out a lock, but
 		# it only has to be a shared one.
-		my $lock = $self->raiilock(1);
+		my $lock = $self->guardlock(1);
 		my $r = syswrite($self->out, $buf);
 		die "write(): $!\n" unless defined $r;
 		# POSIX guarantees that no partial writes will occur
-		confess "short write"
-			if $r < length($buf);
+		confess "short write" if $r < length($buf);
 	}
 }
 
+# When sending an RPC message that requires a reply, we need to
+# do locking on a higher level.
+# In that case this non-locking version of send_rpc must be used.
+
 sub send_rpc_unlocked {
-	# When sending an RPC message that requires a reply, we need to
-	# do locking on a higher level.
-	# In that case this non-locking version of send_rpc must be used.
-	confess("internal error: utf8 data passed to send_rpc()")
-		if utf8::is_utf8($_[1]);
+	confess("internal error: utf8 data passed") if utf8::is_utf8($_[1]);
 	my $buf = pack('LC', length($_[1]), $_[0]).$_[1];
 	my $r = syswrite($self->out, $buf);
 	die "write(): $!\n" unless defined $r;
 	# POSIX guarantees that no partial writes will occur
-	die "short write\n"
-		if $r < length($buf);
+	confess "short write" if $r < length($buf);
 }
 
 sub recv_rpc {
@@ -219,7 +276,7 @@ sub checksumSeed {
 
 sub attribGet {
 	my $attrs = shift;
-	my $lock = $self->raiilock;
+	my $lock = $self->guardlock;
 	$self->send_rpc_unlocked(RSYNC_RPC_attribGet, serialize_attrs($attrs));
 	my $res = $self->recv_rpc;
 	return $res eq '' ? undef : parse_attrs($res);
@@ -256,7 +313,7 @@ sub csumStart {
 sub csumGet {
 	my ($num, $csumLen, $blockSize) = @_;
 	return unless $self->needMD4_isset;
-	my $lock = $self->raiilock;
+	my $lock = $self->guardlock;
 	$self->send_rpc_unlocked(RSYNC_RPC_csumGet, pack('QLC', $num, $blockSize, $csumLen));
 	return $self->recv_rpc;
 }
@@ -264,7 +321,7 @@ sub csumGet {
 sub csumEnd {
 	return unless $self->needMD4_isset;
 	if($self->needMD4) {
-		my $lock = $self->raiilock;
+		my $lock = $self->guardlock;
 		$self->send_rpc_unlocked(RSYNC_RPC_csumEnd_digest, '');
 		return $self->recv_rpc;
 	} else {
