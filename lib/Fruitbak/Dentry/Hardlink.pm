8ae75d8386e96f813829a80292c0ac8be6a6c767
Wessel Dankers <wsl@fruit.je>
2016-01-22 15:30:02 +0100

    fix Dentry::Hardlink typos and parens

--- lib/Fruitbak/Dentry/Hardlink.pm
+++ lib/Fruitbak/Dentry/Hardlink.pm
@@ -2,7 +2,7 @@
 
 =head1 NAME
 
-Fruitbak::Dentry - bookkeeping for hardlinked filesystem entries
+Fruitbak::Dentry::Hardlink - bookkeeping for hardlinked filesystem entries
 
 =head1 AUTHOR
 
@@ -36,119 +36,119 @@ field target;
 field original;
 
 sub name {
-	confess "attempt to modify a Fruitbak::Dentry::Hardlink directly" if @_;
+	confess("attempt to modify a Fruitbak::Dentry::Hardlink directly") if @_;
 	return $self->original->name;
 }
 
 sub inode {
-	confess "attempt to modify a Fruitbak::Dentry::Hardlink directly" if @_;
+	confess("attempt to modify a Fruitbak::Dentry::Hardlink directly") if @_;
 	return $self->target->inode;
 }
 
 sub mode {
-	confess "attempt to modify a Fruitbak::Dentry::Hardlink directly" if @_;
+	confess("attempt to modify a Fruitbak::Dentry::Hardlink directly") if @_;
 	return $self->target->mode;
 }
 
 sub size {
-	confess "attempt to modify a Fruitbak::Dentry::Hardlink directly" if @_;
+	confess("attempt to modify a Fruitbak::Dentry::Hardlink directly") if @_;
 	return $self->target->size;
 }
 
 sub storedsize {
-	confess "attempt to modify a Fruitbak::Dentry::Hardlink directly" if @_;
+	confess("attempt to modify a Fruitbak::Dentry::Hardlink directly") if @_;
 	return $self->target->storedsize;
 }
 
 sub mtime_ns {
-	confess "attempt to modify a Fruitbak::Dentry::Hardlink directly" if @_;
+	confess("attempt to modify a Fruitbak::Dentry::Hardlink directly") if @_;
 	return $self->target->mtime_ns;
 }
 
 sub uid {
-	confess "attempt to modify a Fruitbak::Dentry::Hardlink directly" if @_;
+	confess("attempt to modify a Fruitbak::Dentry::Hardlink directly") if @_;
 	return $self->target->uid;
 }
 
 sub gid {
-	confess "attempt to modify a Fruitbak::Dentry::Hardlink directly" if @_;
+	confess("attempt to modify a Fruitbak::Dentry::Hardlink directly") if @_;
 	return $self->target->gid;
 }
 
 sub digests {
-	confess "attempt to modify a Fruitbak::Dentry::Hardlink directly" if @_;
+	confess("attempt to modify a Fruitbak::Dentry::Hardlink directly") if @_;
 	return $self->target->digests(@_);
 }
 
 sub hardlink {
-	confess "attempt to modify a Fruitbak::Dentry::Hardlink directly" if @_;
+	confess("attempt to modify a Fruitbak::Dentry::Hardlink directly") if @_;
 	return $self->target->name;
 }
 
 sub symlink {
-	confess "attempt to modify a Fruitbak::Dentry::Hardlink directly" if @_;
+	confess("attempt to modify a Fruitbak::Dentry::Hardlink directly") if @_;
 	return $self->target->name;
 }
 
 sub rdev {
-	confess "attempt to modify a Fruitbak::Dentry::Hardlink directly" if @_;
+	confess("attempt to modify a Fruitbak::Dentry::Hardlink directly") if @_;
 	return $self->target->rdev;
 }
 
 sub rdev_minor {
-	confess "attempt to modify a Fruitbak::Dentry::Hardlink directly" if @_;
+	confess("attempt to modify a Fruitbak::Dentry::Hardlink directly") if @_;
 	return $self->target->rdev_minor;
 }
 
 sub rdev_major {
-	confess "attempt to modify a Fruitbak::Dentry::Hardlink directly" if @_;
+	confess("attempt to modify a Fruitbak::Dentry::Hardlink directly") if @_;
 	return $self->target->rdev_major;
 }
 
 sub extra {
-	confess "attempt to modify a Fruitbak::Dentry::Hardlink directly" if @_;
+	confess("attempt to modify a Fruitbak::Dentry::Hardlink directly") if @_;
 	return $self->target->extra;
 }
 
 use constant is_hardlink => 1;
 
 sub is_file {
-	confess "attempt to modify a Fruitbak::Dentry::Hardlink directly" if @_;
+	confess("attempt to modify a Fruitbak::Dentry::Hardlink directly") if @_;
 	return $self->target->is_file;
 }
 
 sub is_directory {
-	confess "attempt to modify a Fruitbak::Dentry::Hardlink directly" if @_;
+	confess("attempt to modify a Fruitbak::Dentry::Hardlink directly") if @_;
 	return $self->target->is_directory;
 }
 
 sub is_symlink {
-	confess "attempt to modify a Fruitbak::Dentry::Hardlink directly" if @_;
+	confess("attempt to modify a Fruitbak::Dentry::Hardlink directly") if @_;
 	return $self->target->is_symlink;
 }
 
 sub is_device {
-	confess "attempt to modify a Fruitbak::Dentry::Hardlink directly" if @_;
+	confess("attempt to modify a Fruitbak::Dentry::Hardlink directly") if @_;
 	return $self->target->is_device;
 }
 
 sub is_chardev {
-	confess "attempt to modify a Fruitbak::Dentry::Hardlink directly" if @_;
+	confess("attempt to modify a Fruitbak::Dentry::Hardlink directly") if @_;
 	return $self->target->is_chardev;
 }
 
 sub is_blockdev {
-	confess "attempt to modify a Fruitbak::Dentry::Hardlink directly" if @_;
+	confess("attempt to modify a Fruitbak::Dentry::Hardlink directly") if @_;
 	return $self->target->is_blockdev;
 }
 
 sub is_fifo {
-	confess "attempt to modify a Fruitbak::Dentry::Hardlink directly" if @_;
+	confess("attempt to modify a Fruitbak::Dentry::Hardlink directly") if @_;
 	return $self->target->is_fifo;
 }
 
 sub is_socket {
-	confess "attempt to modify a Fruitbak::Dentry::Hardlink directly" if @_;
+	confess("attempt to modify a Fruitbak::Dentry::Hardlink directly") if @_;
 	return $self->target->is_socket;
 }
 
