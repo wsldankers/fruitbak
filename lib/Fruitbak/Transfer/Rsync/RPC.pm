120b5f30b9877c74d97af685f02a41f08a0b3b65
Wessel Dankers <wsl@fruit.je>
2016-02-24 17:22:13 +0100

    clearer error message

--- lib/Fruitbak/Transfer/Rsync/RPC.pm
+++ lib/Fruitbak/Transfer/Rsync/RPC.pm
@@ -93,7 +93,7 @@ sub saferead {
 	while($len < $num) {
 		my $r = sysread $fh, $res, $num - $len, $len;
 		die "read(): $!\n" unless defined $r;
-		confess("short read ($r < 1)") unless $r;
+		confess("short read ($len < $num)") unless $r;
 		$len = length($res);
 	}
 	return $res;
