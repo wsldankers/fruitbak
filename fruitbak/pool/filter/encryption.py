4a6b635d007439ae0e5e9189b81d70fea9836de0
Wessel Dankers <wsl@fruit.je>
2019-09-09 20:00:23 +0200

    pool.filter.encryption: tidy up

--- fruitbak/pool/filter/encryption.py
+++ fruitbak/pool/filter/encryption.py
@@ -41,6 +41,14 @@ class Encrypt(Filter):
 		# we get away with reusing this because the mode is ECB
 		return AES.new(self.validated_key)
 
+	@initializer
+	def encrypt_hash(self):
+		return self.aes.encrypt
+
+	@initializer
+	def decrypt_hash(self):
+		return self.aes.decrypt
+
 	@locked
 	@initializer
 	def box(self):
@@ -48,44 +56,44 @@ class Encrypt(Filter):
 
 	@locked
 	@initializer
-	def encrypt(self):
+	def encrypt_chunk(self):
 		box = self.box
 		nonce = nacl.utils.random(box.NONCE_SIZE)
-		return lambda value: box.encrypt(value, nonce)
+		return lambda chunk: box.encrypt(chunk, nonce)
 
 	@initializer
-	def decrypt(self):
+	def decrypt_chunk(self):
 		return self.box.decrypt
 
 	def get_chunk(self, callback, hash):
 		cpu_executor = self.cpu_executor
 
-		def when_done(value, exception):
-			if value is None:
-				callback(value, exception)
+		def when_done(chunk, exception):
+			if chunk is None:
+				callback(chunk, exception)
 			else:
-				decrypt = self.decrypt
+				decrypt_chunk = self.decrypt_chunk
 				def job():
 					try:
-						d = decrypt(value)
+						d = decrypt_chunk(chunk)
 					except:
 						callback(None, exc_info())
 					else:
 						callback(d, exception)
 				cpu_executor.submit(job)
 
-		return super().get_chunk(when_done, self.aes.encrypt(bytes(hash)))
+		return super().get_chunk(when_done, self.encrypt_hash(bytes(hash)))
 
-	def put_chunk(self, callback, hash, value):
+	def put_chunk(self, callback, hash, chunk):
 		cpu_executor = self.cpu_executor
 		subordinate = self.subordinate
-		encrypt = self.encrypt
+		encrypt_chunk = self.encrypt_chunk
 
 		hash = self.aes.encrypt(bytes(hash))
 
 		def job():
 			try:
-				c = encrypt(value)
+				c = encrypt_chunk(chunk)
 			except:
 				callback(exc_info())
 			else:
@@ -93,7 +101,13 @@ class Encrypt(Filter):
 
 		cpu_executor.submit(job)
 
+	def has_chunk(self, callback, hash):
+		return super().has_chunk(callback, self.encrypt_hash(bytes(hash)))
+
+	def del_chunk(self, callback, hash):
+		return super().del_chunk(callback, self.encrypt_hash(bytes(hash)))
+
 	def lister(self, agent):
-		decrypt = self.aes.decrypt
+		decrypt_hash = self.decrypt_hash
 		for hash in self.subordinate.lister(agent):
-			yield decrypt(hash)
+			yield decrypt_hash(hash)
