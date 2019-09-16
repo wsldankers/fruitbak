18f1f19923f073594e662dc48ada5df4fa2351e0
Wessel Dankers <wsl@fruit.je>
2019-09-16 20:22:29 +0200

    make lmdb optional

--- fruitbak/pool/storage/__init__.py
+++ fruitbak/pool/storage/__init__.py
@@ -4,8 +4,13 @@ class Storage(Handler):
 	pass
 
 from fruitbak.pool.storage.filesystem import Filesystem
+
 try:
 	from fruitbak.pool.storage.filesystem import LinuxFilesystem
 except ImportError:
 	pass
-from fruitbak.pool.storage.lmdb import LMDB
+
+try:
+	from fruitbak.pool.storage.lmdb import LMDB
+except ImportError:
+	pass
