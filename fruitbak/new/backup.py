43a87c51b1de0e80cc7cb8d490bca558ca86bd97
Wessel Dankers <wsl@fruit.je>
2022-06-24 15:44:09 +0200

    new.backup: add docstrings and rename properties to appease linter

--- fruitbak/new/backup.py
+++ fruitbak/new/backup.py
@@ -12,6 +12,9 @@ from fruitbak.new.share import NewShare
 from fruitbak.transfer import LocalTransfer
 
 def _declass(value):
+	"""Turn configuration items that were expressed using Python class syntax
+	into regular `dict`s. Other values are returned unmodified."""
+
 	if type(value) is type:
 		return {
 			key: val
@@ -24,34 +27,80 @@ def _declass(value):
 class NewBackup(Initializer):
 	@initializer
 	def fruitbak(self):
+		"""The main `Fruitbak` object. Defaults to the host's `Fruitbak` object.
+
+		:type: fruitbak.Fruitbak"""
+
 		return self.host.fruitbak
 
 	@initializer
 	def pool(self):
+		"""The pool which will be used to store file data. Defaults to the
+		pool of the main `Fruitbak` object.
+
+		:type: fruitbak.pool.Pool"""
+
 		return self.fruitbak.pool
 
 	@initializer
 	def agent(self):
+		"""The pool agent which will be used to store file data. Defaults to
+		requesting a new agent from the pool of this backup.
+
+		:type: fruitbak.pool.PoolAgent"""
+
 		return self.pool.agent()
 
 	@initializer
 	def config(self):
+		"""The host's configuration object.
+
+		:type: fruitbak.Config"""
+
 		return self.host.config
 
 	@configurable
 	def share(self):
+		"""Default values for all shares. Deprecated, do not use.
+		Default values can be set on the host instead. Defaults
+		to an empty `dict`.
+
+		:type: dict"""
+
 		return {}
 
 	@share.prepare
-	def share(self, value):
+	def share_prepare(self, value):
+		"""Prepare the default share values for use. It can optionally be
+		configured using class syntax so convert it to a normal dict if
+		necessary.
+
+		:return: The normalized share value
+		:rtype: dict"""
+
 		return _declass(value)
 
 	@configurable
 	def shares(self):
+		"""Configured shares for this host. Can be a list or a dict,
+		if it's a dict the keys form the name of the share. In either
+		case the values must be dicts with parameters for each share.
+		Both the outer as well as the inner dicts can be specified
+		using Python class syntax instead.
+
+		Defaults to a single share called "root" with path "/".
+
+		:type: list or dict"""
+
 		return [{'name': 'root', 'path': '/'}]
 
 	@shares.prepare
-	def shares(self, value):
+	def shares_prepare(self, value):
+		"""Prepare the list of shares for use. Any classes are converted
+		to dicts; an outer dict is converted to a list.
+
+		:rtype: list"""
+
 		share = self.share
 		value = _declass(value)
 		if isinstance(value, Mapping):
@@ -60,48 +109,131 @@ class NewBackup(Initializer):
 			return [{**share, **_declass(v)} for v in value]
 
 	@configurable_command
+	def before_host(self):
+		"""An optional command to run before the shares of this host
+		are backed up.
+
+		:type: callable"""
+
+		return self.pre_command
+
+	@configurable_command
 	def pre_command(self):
+		"""An optional command to run before the shares of this host
+		are backed up. Deprecated, use `before_host` instead.
+
+		:type: callable"""
+
 		return xyzzy
 
 	@configurable_command
+	def after_host(self):
+		"""An optional command to run after the shares of this host
+		are backed up.
+
+		:type: callable"""
+
+		return self.post_command
+
+	@configurable_command
 	def post_command(self):
+		"""An optional command to run after the shares of this host
+		are backed up. Deprecated, use `after_host` instead.
+
+		:type: callable"""
+
 		return xyzzy
 
 	@configurable
 	def transfer_method(self):
+		"""The transfer method for all shares of this host.
+
+		Should be a constructor for a `fruitbak.transfer.Transfer` subclass.
+		If `transfer` is not set, it will be initialized by calling this
+		constructor with `newshare` set to this share object and any
+		keyword arguments from the `transfer_options` dict.
+
+		Defaults to `fruitbak.transfer.LocalTransfer`."""
+
 		return LocalTransfer
 
 	@configurable
 	def transfer_options(self):
+		"""Options for `transfer_method`.
+
+		If `transfer` is not set, it will be initialized by calling the
+		`transfer_method` constructor with the contents of `transfer_options`
+		as keyword arguments.
+
+		:type: dict"""
+
 		return {}
 
 	@configurable_function
 	def transfer(**kwargs):
+		"""An instance of `fruitbak.transfer.Transfer` that will be called
+		upon to transfer data for this share.
+
+		:type: fruitbak.transfer.Transfer"""
+
 		newshare = kwargs['newshare']
 		return newshare.transfer_method(**newshare.transfer_options, **kwargs)
 
 	@configurable
 	def excludes(self):
+		"""An iterable of excludes (using the generic syntax).
+
+		:type: iterable"""
+
 		return frozenset()
 
 	@initializer
 	def backupdir(self):
+		"""The (relative) name of the directory in which metadata for the
+		new backup will be stored. Defaults to simply "new", it will be
+		renamed to its final name when the backup is completed.
+
+		:type: str or bytes or Path"""
+
 		return Path('new')
 
 	@initializer
 	def backupdir_fd(self):
+		"""File descriptor of the directory in which metadata for this
+		new backup will be stored.
+
+		:type: fd"""
+
 		return self.host.hostdir_fd.sysopendir(self.backupdir, create_ok = True)
 
 	@initializer
 	def sharedir(self):
+		"""The name of the directory (relative to `backupdir`) in which
+		metadata for the shares of the new backup will be stored.
+		Defaults to simply "share".
+
+		:type: str or bytes or Path"""
+
 		return Path('share')
 
 	@initializer
 	def sharedir_fd(self):
+		"""File descriptor of the directory in which metadata for the shares
+		of this new backup will be stored.
+
+		:type: fd"""
+
 		return self.backupdir_fd.sysopendir(self.sharedir, create_ok = True, path_only = True)
 
 	@initializer
 	def predecessor(self):
+		"""The previous backup.
+		This is used as a source of known hashes to prevent unneccesary
+		disk I/O. Returns an empty dict if none could be found, so
+		be sure to only use use indexing operations on it.
+
+		:type: fruitbak.backup.Backup or dict"""
+
 		try:
 			return self.host[-1]
 		except IndexError:
